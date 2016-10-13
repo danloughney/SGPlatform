6fdf1da13b328435de5ce34fd91a5818c4013bf6
Daniel Loughney <dan@spooky.group>
2016-10-13 17:38:42 -0400

    Initial Commit

---
+++ SGPlatform/SGPlatform.h
@@ -0,0 +1,19 @@
+//
+//  SGPlatform.h
+//  SGPlatform
+//
+//  Created by Daniel Loughney on 10/13/16.
+//  Copyright © 2016 SpookyGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SGPlatform.
+FOUNDATION_EXPORT double SGPlatformVersionNumber;
+
+//! Project version string for SGPlatform.
+FOUNDATION_EXPORT const unsigned char SGPlatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGPlatform/PublicHeader.h>
+
+
